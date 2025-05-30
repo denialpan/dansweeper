b72b3658da7b43ff62d3ea062d47f5e373a98090
denial <danpan1232@gmail.com>
2025-05-30 03:28:56 -0400

    Cleaner configuration screen

--- src/main.cpp
+++ src/main.cpp
@@ -8,106 +8,131 @@
 
 using namespace std;
 
-enum GameState {
+enum GameState
+{
     MENU,
     GAME,
 };
 
 const int TEXTUREMAP_TILE_SIZE = 16;
+bool showMessageBox = false;
 
-int main() {
+int main()
+{
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "dansweeper");
     SetTargetFPS(120);
     GuiLoadStyle("styles/default/style_default.rgs");
 
+    Font customFont = LoadFontEx("resources/ProggyClean.ttf", 13, 0, 250);
+    SetTextureFilter(customFont.texture, TEXTURE_FILTER_POINT);
+
     GameState state = MENU;
 
     const Rectangle TILE_RECTS[] = {
-        {0 * 16, 0 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // 1
-        {1 * 16, 0 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // 2
-        {2 * 16, 0 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // 3
-        {3 * 16, 0 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // 4
-        {0 * 16, 1 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // 5
-        {1 * 16, 1 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // 6
-        {2 * 16, 1 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // 7
-        {3 * 16, 1 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // 8
-        {0 * 16, 2 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // blank revealed
-        {1 * 16, 2 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // blank unrevealed
-        {2 * 16, 2 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // flag
-        {3 * 16, 2 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // bomb wrong
-        {0 * 16, 3 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // ? revealed
-        {1 * 16, 3 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // ? unrevealed
-        {2 * 16, 3 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // bomb normal
-        {3 * 16, 3 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE},  // bomb hit
+        {0 * 16, 0 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // 1
+        {1 * 16, 0 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // 2
+        {2 * 16, 0 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // 3
+        {3 * 16, 0 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // 4
+        {0 * 16, 1 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // 5
+        {1 * 16, 1 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // 6
+        {2 * 16, 1 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // 7
+        {3 * 16, 1 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // 8
+        {0 * 16, 2 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // blank revealed
+        {1 * 16, 2 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // blank unrevealed
+        {2 * 16, 2 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // flag
+        {3 * 16, 2 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // bomb wrong
+        {0 * 16, 3 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // ? revealed
+        {1 * 16, 3 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // ? unrevealed
+        {2 * 16, 3 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // bomb normal
+        {3 * 16, 3 * 16, TEXTUREMAP_TILE_SIZE, TEXTUREMAP_TILE_SIZE}, // bomb hit
     };
 
     // Load texture atlas
     Texture2D tileAtlas = LoadTexture("resources/texturemap.png");
 
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose())
+    {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        if (state == MENU) {
-            static int radioActive = 0;  // 0 = Custom config, 1 = Seed
-            static bool useCustomConfig = true;
-            static int inputWidth = 9;
-            static int inputHeight = 9;
-            static int inputBombs = 10;
-            static char seedBuffer[16] = "Xy9z7Aa2";
-            static bool configConfirmed = false;
+        int contentWidth = 240;
+        int contentHeight = 250; // Adjust based on number of elements
+        int screenWidth = GetScreenWidth();
+        int screenHeight = GetScreenHeight();
 
-            DrawText("Configuration", 330, 140, 20, RAYWHITE);
+        int originX = (screenWidth - contentWidth) / 2;
+        int originY = (screenHeight - contentHeight) / 2;
 
-            // Radio toggle between Custom and Seed
-            GuiToggleGroup((Rectangle){300, 170, 200, 24}, "Custom;Seed", &radioActive);
-            useCustomConfig = (radioActive == 0);
+        if (state == MENU)
+        {
 
-            if (useCustomConfig) {
-                // Width
-                float widthTemp = static_cast<float>(inputWidth);
-                GuiSliderBar((Rectangle){380, 210, 150, 20}, NULL, NULL, &widthTemp, 5, 50);
-                inputWidth = static_cast<int>(widthTemp + 0.5f);
+            static int activeTab = 0; // 0 = Manual, 1 = Seed
+            static int gridWidth = 9;
+            static int gridHeight = 9;
+            static int numMines = 10;
 
-                // Height
-                float heightTemp = static_cast<float>(inputHeight);
-                GuiSliderBar((Rectangle){380, 240, 150, 20}, NULL, NULL, &heightTemp, 5, 50);
-                inputHeight = static_cast<int>(heightTemp + 0.5f);
+            static char seedText[17] = ""; // 16-char seed + null terminator
+            static bool confirmPressed = false;
 
-                // Bombs
-                float bombsTemp = static_cast<float>(inputBombs);
-                GuiSliderBar((Rectangle){380, 270, 150, 20}, NULL, NULL, &bombsTemp, 1, 999);
-                inputBombs = static_cast<int>(bombsTemp + 0.5f);
+            static bool useSeed = false; // false = manual, true = seed
 
-                DrawText(TextFormat("%d", inputWidth), 540, 210, 20, RAYWHITE);
-                DrawText(TextFormat("%d", inputHeight), 540, 240, 20, RAYWHITE);
-                DrawText(TextFormat("%d", inputBombs), 540, 270, 20, RAYWHITE);
-            } else {
-                static bool seedEdit = false;
+            // Title
+            GuiLabel((Rectangle){originX, originY, 240, 20}, "dansweeper");
 
-                Rectangle seedBox = {380, 210, 140, 20};
-                if (GuiTextBox(seedBox, seedBuffer, 16, seedEdit)) {
-                    // If textbox is active and Enter is pressed, exit edit mode
-                    seedEdit = false;
-                }
-                if (CheckCollisionPointRec(GetMousePosition(), seedBox) &&
-                    IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                    seedEdit = true;
-                }
+            // Toggle button
+            if (GuiButton((Rectangle){originX, originY + 30, 240, 30},
+                          useSeed ? "Custom" : "Seed"))
+            {
+                useSeed = !useSeed;
             }
 
-            if (GuiButton((Rectangle){330, 320, 140, 40}, "CONFIRM")) {
-                configConfirmed = true;
+            // Manual config
+            if (!useSeed)
+            {
+                float tempGridWidth = (float)gridWidth;
+                float tempGridHeight = (float)gridHeight;
+                float tempNumMines = (float)numMines;
+
+                int maxMines = gridWidth * gridHeight - 1;
+
+                GuiSliderBar((Rectangle){originX, originY + 80, 240, 20}, "Width", TextFormat("%d", gridWidth), &tempGridWidth, 5.0f, 30.0f);
+                GuiSliderBar((Rectangle){originX, originY + 110, 240, 20}, "Height", TextFormat("%d", gridHeight), &tempGridHeight, 5.0f, 30.0f);
+                GuiSliderBar((Rectangle){originX, originY + 140, 240, 20}, "Mines", TextFormat("%d", numMines), &tempNumMines, 1.0f, (float)maxMines);
+
+                gridWidth = (int)(tempGridWidth + 0.5f);
+                gridHeight = (int)(tempGridHeight + 0.5f);
+                numMines = (int)(tempNumMines + 0.5f);
+            }
+            else
+            {
+                GuiLabel((Rectangle){originX, originY + 80, 240, 20}, "Seed Input:");
+                GuiTextBox((Rectangle){originX, originY + 100, 240, 30}, seedText, 17, true);
             }
 
-        } else if (state == GAME) {
+            // Confirm button
+            if (GuiButton((Rectangle){originX, originY + 190, 240, 30}, "Confirm"))
+            {
+                if (useSeed)
+                {
+                    // Use seedText
+                }
+                else
+                {
+                    // Use gridWidth, gridHeight, numMines
+                }
+                state = GAME;
+            }
+        }
+        else if (state == GAME)
+        {
         }
 
         int fps = GetFPS();
         const char *fpsText = TextFormat("%d FPS", fps);
         int fpsTextWidth = MeasureText(fpsText, 20);
-        DrawText(fpsText, GetScreenWidth() - fpsTextWidth - 10, 10, 20, LIME);
+        DrawTextEx(customFont, fpsText, (Vector2){170, 0}, 13, 0, LIME);
+        DrawTextEx(customFont, "CONFIGURATION", (Vector2){0, 0}, 13, 0, LIME);
 
         EndDrawing();
     }
